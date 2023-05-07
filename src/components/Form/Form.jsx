61f6998c42375d8e04e424db153b663731d25d5b
OleksiiKostenko <kostenko.oleksii91@gmail.com>
2023-05-07 19:44:44 +0300

    Added : addComment and getComment

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,22 +1,42 @@
-import React, { useState } from 'react';
-import toast from 'react-hot-toast';
-import { BiMailSend } from 'react-icons/bi';
-import styles from './Form.module.css';
+import React, { useState } from "react";
+import toast from "react-hot-toast";
+import { BiMailSend } from "react-icons/bi";
+import { useAddCommentMutation } from "../../redux/commentApi";
+import styles from "./Form.module.css";
 
 export const Form = () => {
-  const [author, setAuthor] = useState('');
-  const [content, setContent] = useState('');
+  const [author, setAuthor] = useState("");
+  const [content, setContent] = useState("");
+  const [addComment, { isLoading }] = useAddCommentMutation();
 
   const onHandleChange = (e) => {
     const { name, value } = e.target;
-    console.log(name, value);
+    switch (name) {
+      case "name":
+        setAuthor(value);
+        break;
+      case "text":
+        setContent(value);
+      default:
+        break;
+    }
   };
 
   const onHandleSubmit = (e) => {
     e.preventDefault();
+    if (author.trim() === "" || content.trim() === "") {
+      toast.error("All field must by filled");
+      return;
+    }
+    try {
+      addComment({ author, content });
+      toast.success("Good news");
+    } catch (error) {
+      toast.error("Bad news");
+    }
 
-    setAuthor('');
-    setContent('');
+    setAuthor("");
+    setContent("");
   };
 
   return (
@@ -25,8 +45,8 @@ export const Form = () => {
         <label className={styles.label}>
           <span className={styles.labelName}>Full name</span>
           <input
-            type='text'
-            name='name'
+            type="text"
+            name="name"
             className={styles.input}
             value={author}
             onChange={onHandleChange}
@@ -37,8 +57,8 @@ export const Form = () => {
           <span className={styles.labelName}>Your comment</span>
           <textarea
             className={styles.input}
-            name='text'
-            rows='5'
+            name="text"
+            rows="5"
             value={content}
             onChange={onHandleChange}
           ></textarea>
